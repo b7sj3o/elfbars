f58ac31e83097bf72f6d631cd45cbe32575fa769
Vitalik <vitalikfedytnyk@gmail.com>
2023-11-28 00:48:21 +0200

    everything ready

--- js/script.js
+++ js/script.js
@@ -19,24 +19,32 @@ function changeItems(kind) {
 
 let moneyElement = document.querySelector('.earned-money');
 let spentElement = document.querySelector('.spent-money');
+let difference = document.querySelector('.diff');
+let soldAmountTag = document.querySelector('.sold-amount');
 
 let EarnedMoney = 0;
 let SpentMoney = 0;
+let soldAmount = 0;
 document.querySelectorAll('.counter-container').forEach(container => {
     const decrementButton = container.querySelector('.decrement');
     const counterElement = container.querySelector('.counter');
     const incrementButton = container.querySelector('.increment');
     const counterId = container.dataset.counter;
 
+    const sellPriceInput = container.querySelector('.sell-price');
+    const buyPriceInput = container.querySelector('.buy-price');
+
+    sellPriceInput.value = localStorage.getItem(`sellPrice_${counterId}`) || 0;
+    buyPriceInput.value = localStorage.getItem(`buyPrice_${counterId}`) || 0;
+
+    
     // Initialize counter value
     counterElement.value = localStorage.getItem(`counter_${counterId}`) || 0;
-    const sellPrice = parseInt(localStorage.getItem(`sellPrice_${counterId}`)) || 0;
-    const buyPrice = parseInt(localStorage.getItem(`buyPrice_${counterId}`)) || 0;
 
-    EarnedMoney += sellPrice * counterElement.value;
-    SpentMoney += buyPrice * counterElement.value;
-    
-    
+    soldAmount += Number(counterElement.value);
+
+    EarnedMoney += sellPriceInput.value * counterElement.value;
+    SpentMoney += buyPriceInput.value * counterElement.value;
     
     decrementButton.addEventListener('click', () => {
         const currentValue = parseInt(counterElement.value);
@@ -51,11 +59,23 @@ document.querySelectorAll('.counter-container').forEach(container => {
         counterElement.value = currentValue + 1;
         localStorage.setItem(`counter_${counterId}`, currentValue + 1);
     });
+
+    sellPriceInput.addEventListener('change', () => {
+        console.log(sellPriceInput.value)
+        localStorage.setItem(`sellPrice_${counterId}`, sellPriceInput.value)
+        
+    });
+
+    buyPriceInput.addEventListener('change', () => {
+        console.log(buyPriceInput.value)
+        localStorage.setItem(`buyPrice_${counterId}`, buyPriceInput.value)
+    });
 });
 
-moneyElement.innerHTML = `EARNED MONEY: ${EarnedMoney}`
-spentElement.innerHTML = `SPENT MONEY: ${SpentMoney}`
-document.querySelector('.diff').innerHTML = `DIFFERENT: ${EarnedMoney-SpentMoney}`
+moneyElement.innerHTML = `Загальний оборот: ${EarnedMoney}₴`
+spentElement.innerHTML = `Чистий дохід: ${EarnedMoney-SpentMoney}₴`
+difference.innerHTML = `Різниця: ${SpentMoney}₴`;
+soldAmountTag.innerHTML = `Продано: ${soldAmount} шт`;
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -63,14 +83,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const scrollUpButton = document.getElementById('scrollUpButton');
 
     scrollDownButton.addEventListener('click', function () {
-        // Scroll smoothly to the bottom of the page
         window.scrollTo({
             top: document.body.scrollHeight,
             behavior: 'smooth'
         });
     });
     scrollUpButton.addEventListener('click', function () {
-        // Scroll smoothly to the bottom of the page
         window.scrollTo({
             top: document.body.scrollTop,
             behavior: 'smooth'
@@ -79,8 +97,11 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// ADD EVENT LISTENER - sell-price / buy-price
+function clearData() {
+    localStorage.clear();
+    location.reload();
+}
 
-// localStorage.setItem(id, value);
-// value = localStorage.getItem(id)
-
+function reloadData() {
+    location.reload();
+}
